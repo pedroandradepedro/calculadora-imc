196116d18d299dfb410205f60cb6e69e359badfb
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-07-04 15:47:04 -0300

    Terminando de estilizar o CSS e adequando o JS para cada condição

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let peso;
 let altura;
 let imc;
 let resultado;
+let background;
 
 function calcular(event) {
   event.preventDefault();
@@ -9,20 +10,30 @@ function calcular(event) {
   peso = document.getElementById("peso").value;
   altura = document.getElementById("altura").value;
 
+  background = document.querySelector(".right");
+
   imc = (peso / (altura * altura)).toFixed(2);
 
   resultado = document.getElementById("resultado");
 
+  resultado.style.color = "";
+
   if (imc < 17) {
-    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}.<br/> Você está MUITO ABAIXO DO PESO!`;
+    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}<br/> Você está MUITO ABAIXO DO PESO!`;
+    background.style.backgroundColor = "red";
   } else if (imc >= 17 && imc <= 18.49) {
-    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}.<br/> Você está ABAIXO DO PESO!`;
+    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}<br/> Você está ABAIXO DO PESO!`;
+    background.style.background = "#f7c900";
+    resultado.style.color = "#242424";
   } else if (imc >= 18.5 && imc <= 24.99) {
-    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}.<br/> Você está no PESO IDEAL!`;
+    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}<br/> Você está no PESO IDEAL!`;
+    background.style.background = "green";
   } else if (imc >= 25 && imc <= 29.99) {
-    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}.<br/> Você está ACIMA DO PESO!`;
+    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}<br/> Você está ACIMA DO PESO!`;
+    background.style.background = "red";
   } else {
-    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}.<br/> Você está OBESO!`;
+    resultado.innerHTML = `<br/><br/>O resultado do seu IMC foi: ${imc}<br/> Você está OBESO!`;
+    background.style.background = "red";
   }
 
   document.getElementById("peso").value = "";
